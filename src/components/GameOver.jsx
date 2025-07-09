9254c31251271e9d7283fd698e04b5288c062e43
Anand Agarwal <100064537+anand20agarwal@users.noreply.github.com>
2025-07-09 19:28:57 +0530

    Update GameOver.jsx

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -5,7 +5,7 @@ function GameOver({winner, onRestart}) {
     <div id="game-over">
       <h2>Game Over!</h2>
       {winner && <p>{winner} won!</p>}
-      {!winner && <p>it is a drwa</p>}
+      {!winner && <p>it is a draw</p>}
       <p><button onClick={onRestart}>Rematch!</button>
       </p> 
     </div>
